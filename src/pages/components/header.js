5e79515155c956631406013f4fa911531fce77ca
TeflonTrout <jtkazo@gmail.com>
2021-01-14 11:57:31 -0500

    updated styling for hamburger menu

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,39 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import headerStyles from './header.module.css'
+import { FaBars } from 'react-icons/fa'
 
 const Header = () => {
-    const data = useStaticQuery(graphql`
-        query {
-            site {
-                siteMetadata{
-                    title
-                }
-            }
+    // const data = useStaticQuery(graphql`
+    //     query {
+    //         site {
+    //             siteMetadata{
+    //                 title
+    //             }
+    //         }
+    //     }
+    // `)
+
+    const [navbarState, setNavbarState] = useState(false)
+        
+    const navbarToggle = (e) => {
+        setNavbarState(prevState => !prevState)
+    };
+
+    const handleKeyDown = (ev) => {
+        if (ev.keyCode === 13) {
+            return ""
         }
-    `)
+    };
 
     return (
         <div>
             <header>
                 <nav>
-                    <ul className={headerStyles.ul}>
-                        <h1 className={headerStyles.headingTitle}>
-                            <Link className={headerStyles.headingTitle} to='/'>{data.site.siteMetadata.title}</Link>
-                        </h1>
-                        {/* <li className={headerStyles.li}>
-                            <Link className={headerStyles.link} to='/'>Home</Link>
-                        </li> */}
-                        <li className={headerStyles.li}>
-                            <Link className={headerStyles.link} to='/about'>About</Link>
-                        </li>
-                        <li className={headerStyles.li}>
-                            <Link className={headerStyles.link} to='/brewMethods'>Brew Methods</Link>
-                        </li>
-                        <li className={headerStyles.li}>
-                            <Link className={headerStyles.link} to='/blog'>Blog</Link>
-                        </li>
-                    </ul>
+                    <div className={headerStyles.navbarContainer}>
+                        <h1 className={headerStyles.headingTitle}>Caffeinated Trout</h1>
+                        <div 
+                            style={{outline: 'none'}}
+                            className={headerStyles.hamburger}
+                            role="navigation" 
+                            tabIndex={0} 
+                            onClick={(e) => navbarToggle()} 
+                            onKeyDown={(ev) => handleKeyDown()}>
+                            <FaBars />
+                        </div>
+                    </div>
+                    {navbarState ? 
+                    <div>
+                        <ul className={headerStyles.ul}>
+                            <li className={headerStyles.li}>
+                                <Link className={headerStyles.link} to='/'>Home</Link>
+                            </li>
+                            <li className={headerStyles.li}>
+                                <Link className={headerStyles.link} to='/about'>About</Link>
+                            </li>
+                            <li className={headerStyles.li}>
+                                <Link className={headerStyles.link} to='/brewMethods'>Brew Methods</Link>
+                            </li>
+                            <li className={headerStyles.li}>
+                                <Link className={headerStyles.link} to='/blog'>Blog</Link>
+                            </li>
+                        </ul>
+                    </div> : ""}
                 </nav>
             </header>
         </div>
